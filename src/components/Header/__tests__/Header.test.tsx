6a465c03c239766a86141c58f6a1dd64606ad7cf
Matheus <sousamatheus@brq.com>
2024-01-11 17:58:30 -0300

    fix: teste block branch main

--- src/components/Header/__tests__/Header.test.tsx
+++ src/components/Header/__tests__/Header.test.tsx
@@ -110,7 +110,7 @@ describe('Header Tests', () => {
     });
   });
 
-  it('should be press button Settings on modal option and navigate to route Settingss', async () => {
+  it('should be press button Settings on modal option and navigate to route Settings', async () => {
     render(
       renderHeader({
         showArrow: false,
